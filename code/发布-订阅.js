c8c124ff8fecf5596183d2f9172f903d72694563
zhoujg <156052727372@163.com>
2020-05-11 17:35:54 +0800

    发布订阅

--- code/发布-订阅.js
+++ code/发布-订阅.js
@@ -15,31 +15,41 @@ class Dep {
     if (!this.eventBus[event]) {
       this.eventBus[event] = [];
     }
-    this.eventBus[event].push(callback);
+    this.eventBus[event].push(callback.bind());
     // 链式调用
-    return this
+    return this;
   }
   // 取消订阅
   unsubscrible(event, callback) {
     if (
-        typeof event !== "string" &&
-        Object.prototype.toString.call(callback) !== "[object Function]"
-      ) {
-        throw new Error("传入参数格式错误");
-      }
-    if (!this.eventBus[event] || this.eventBus[event].length===0) {
-        throw new Error("没有事件可取消订阅")
+      typeof event !== "string" &&
+      Object.prototype.toString.call(callback) !== "[object Function]"
+    ) {
+      throw new Error("传入参数格式错误");
     }
-    this.eventBus[event].forEach(item,index,arr)
-      // 链式调用
-    return this
+    if (!this.eventBus[event] || this.eventBus[event].length === 0) {
+      throw new Error("没有事件可取消订阅");
+    }
+    this.eventBus[event].forEach((item, index, arr) => {
+      if (item === callback) {
+        arr.splice(index, 1);
+      }
+    });
+    // 链式调用
+    return this;
   }
   // 通知发布
-  notify(event, callback) {}
+  notify(event) {
+    const args = [...arguments].slice(1);
+    this.eventBus[event].forEach((callback) => {
+      callback.apply(this, args);
+    });
+  }
 }
 let dep = new Dep();
-dep.subscrible("click", () => {
-  console.log(1);
-});
 
-console.log(dep);
+const event1 = function () {
+  console.log("event1触发");
+};
+
+dep.subscrible("click", event1).notify("click");
